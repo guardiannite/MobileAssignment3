aa51c468d50dd8220a0d137c60b98f2fa9a4cb95
00erik2 <erik.hattervig@gmail.com>
2013-11-11 20:44:53 -0700

    Added Forecast Fragment to Main Activity
    The Forecast Fragment is now displayed on creation of app.

--- src/com/example/schultzhattervigweatherviewer/MainActivity.java
+++ src/com/example/schultzhattervigweatherviewer/MainActivity.java
@@ -2,17 +2,31 @@ package com.example.schultzhattervigweatherviewer;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.FragmentManager;
 import android.text.TextUtils;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
-	    
+	
+	private FragmentManager _fragmentManager;
+	private FragmentForecast _fragmentForecast;
+	private final String FRAGMENT_FORECAST_TAG = "ForecastTag";
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
+            _fragmentManager = getFragmentManager();
+            
+            
+            _fragmentForecast = (FragmentForecast) _fragmentManager.findFragmentByTag(FRAGMENT_FORECAST_TAG);
+            if (_fragmentForecast == null)
+            {
+            	_fragmentForecast = new FragmentForecast();
+            }
+            
+            _fragmentManager.beginTransaction().replace(R.id.fragmentContainerFrame, _fragmentForecast, FRAGMENT_FORECAST_TAG ).commit();
             
             // Get City array from resources.
             //_citiesArray = getResources().getStringArray(R.array.cityArray);
@@ -32,6 +46,8 @@ public class MainActivity extends Activity {
             //                ForecastFragment.setArguments(bundle);
             
             // HINT: FragmentManager().beginTransaction()
+    	
+    		
             
     }
 
